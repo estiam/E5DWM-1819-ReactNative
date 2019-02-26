a7a1303a06530ad0f91ab5013db55d0652000128
Sion GENDERS <sion.genders@estiam.com>
2019-02-26 14:50:36 +0100

    Added footer tab

--- interfaces/Home.js
+++ interfaces/Home.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
 import { Text, View, StatusBar, StyleSheet, Platform } from 'react-native';
-import { Container, Header, Content, Footer, Left, Body, Right, Title } from 'native-base';
+import { Container, Header, Icon, Content, Footer, Button, Left, Body, Right, Title, FooterTab } from 'native-base';
 
 class Home extends Component {
   constructor(props) {
     super(props);
   }
+  static navigationOptions = {
+    title: 'my.genius'
+  }
+
   render() {
     return (
       <Container>
-        <Header>
-          <StatusBar/>
-          <Left></Left>
-          <Body>
-            <Title>my.genius</Title>
-          </Body>
-          <Right></Right>
-        </Header>
         <Content></Content>
-        <Footer></Footer>
+        <Footer>
+          <FooterTab>
+            <Button>
+              <Icon name='calendar' />
+              <Text>Mon Planning</Text>
+            </Button>
+            <Button>
+              <Icon name='star-half' />
+              <Text>Mes Notes</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     );
   }
